e7a2567f5bd500331f00dbfe05b46a0a5be631cd
imam ahdy sabilla <112673843+Imamahdysabilla@users.noreply.github.com>
2023-10-03 22:24:29 +0700

    Add files via upload

---
+++ Imamm.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package imamm;
+import java.util.Scanner;
+/**
+ *
+ * @author imran sukron hamid
+ */
+public class Imamm {
+    public static void main(String[] args) {
+        Scanner m = new Scanner(System.in);
+//     int n = m.nextInt();
+//     m.nextLine();
+//     String l [] = new String[n];
+//     for (int k=0;k<n;k++){
+//        l[k] = m.nextLine();
+//     }
+//     for(String j : l){
+//         System.out.print(j+" ");
+//     }
+
+//String s="imam";
+//int x = m.nextInt();
+//int y = m.nextInt();
+//String result = s.substring(x,y);
+//System.out.println(result);
+
+//String str1 = m.nextLine();
+//String str2 = m.nextLine();
+//if (str1.contains(str2)){
+// System.out.println("ya");
+//       }else{
+//    System.out.println("tidak");
+//}
+}
+}
+    
